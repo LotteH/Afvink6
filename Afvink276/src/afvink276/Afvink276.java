0a22789f848929ffabc228910c16f891752d7f8e
Lotte Houwen <lotte@145.102.220.54>
2018-03-28 14:51:45 +0200

    GUI aangemaakt, zonder builder. # startOfSomethingNew

--- Afvink276/src/afvink276/Afvink276.java
+++ Afvink276/src/afvink276/Afvink276.java
@@ -5,17 +5,38 @@
  */
 package afvink276;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  *
  * @author lotte
  */
-public class Afvink276 {
+public class Afvink276 extends JFrame implements ActionListener {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Afvink276 frame = new Afvink276();
+        frame.setSize(1000, 600);
+        frame.setTitle("VirusGUI");
+        frame.createGUI();
+        frame.show();// TODO code application logic here
     }
-    
+
+    public void createGUI() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container window = getContentPane();
+        window.setLayout(new FlowLayout());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
